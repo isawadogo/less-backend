a2a0633ebcb8d20cb528aa06b8642caba386cd93
Ypolites <manon.bonnafous@outlook.com>
2024-08-12 12:17:45 -0400

    enregistrement Manon

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,9 @@
+//appel de la dépendance Mongoose
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING;
+//connexion à la base de donnée
+const connectionString = 'mongodb+srv://admin:31830@clusterbam.4mwajc8.mongodb.net/API_produits';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
-
+	.then(() => console.log('Database connected'))
+	.catch(error => console.error(error))
