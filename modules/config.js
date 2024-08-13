d93d96730da607b0635c4b4f39353ba172155a68
isawadogo <sidbewende15@gmail.com>
2024-08-13 11:50:17 +0200

    update enseigne backend to use vercel deployments URL

--- modules/config.js
+++ modules/config.js
@@ -1,24 +1,24 @@
-const produitBackend = 'http://127.0.0.1:3100/produit';
+const produitBackend = 'https://less-backend-magasin.vercel.app/produit';
 
 const enseignesList = [ 
     {   id: 'enseigne1', 
         nom: 'Leclerc',
-        backendURL: 'http://127.0.0.1:3100/enseigne1'
+        backendURL: 'https://less-backend-magasin.vercel.app/enseigne1'
     }, 
     {
         id: 'enseigne2',
         nom: 'Naturalia',
-        backendURL: 'http://127.0.0.1:3100/enseigne2'
+        backendURL: 'https://less-backend-magasin.vercel.app/enseigne2'
     }, 
     {
         id: 'enseigne3',
         nom: 'Monoprix',
-        backendURL: 'http://127.0.0.1:3100/enseigne3'
+        backendURL: 'https://less-backend-magasin.vercel.app/enseigne3'
     }, 
     {
         id: 'enseigne4',
         nom: 'Leaderprice',
-        backendURL: 'http://127.0.0.1:3100/enseigne4'
+        backendURL: 'https://less-backend-magasin.vercel.app/enseigne4'
     } 
 ];
 
