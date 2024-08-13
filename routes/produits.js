ffe5e2990ae33a342e9aac1c5598984d1386491d
isawadogo <sidbewende15@gmail.com>
2024-08-14 00:35:52 +0200

    Add routes for produits

--- routes/produits.js
+++ routes/produits.js
@@ -33,7 +33,7 @@ router.get('/categories',
       json.categories.map((c) => {if (!categoriesList.includes(c)) {categoriesList.push(c)}})
     }
   }
-   res.json({result: true, categories: categoriesList });
+  res.json({result: true, categories: categoriesList });
 });
 
 /*
@@ -46,47 +46,25 @@ Description : This route returns all the products for a given category
 */
 router.get('/categories/:id', 
   async function (req, res, next) {
-  // validate params: email and password are mandatory
-    /*const result = validationResult(req);
-    if (!result.isEmpty()) {
-      res.json({result: false, error: result.array()})
-      return
-    }*/
-    try {
-      const excludedFields = { 
-        __v: false,
-        _id: false,
-        motDePasse: false,
-      };
-      const userDetails = await User.find({
-        _id: req.body.userId
-      }, excludedFields);
-      
-      if (userDetails.length === 0){
-        res.json({result: false, error: `User with id ${req.body.userId} does not exist`});
-        return;
-      }
 
-      const mergedData = userDetails[0];
-      Object.assign(mergedData, req.body);
-      const updatedData = await User.updateOne(
-        { _id: req.body.userId },
-        mergedData
-      );
-      if ( updatedData.acknowledged) {
-        const updateUserData = await User.find({
-          _id: req.body.userId
-        }, excludedFields);
-        res.json({ result: true, user: updateUserData[0] });
-      } else {
-        res.json({ result: false, message: "No User have been updated" });
+  let categoriesList = [];
+  const excludedFields = { 
+    __v: false,
+   };
+  for (const ens of enseignesList) {
+    const resTemp = await fetch(ens.backendURL + '/categories/' + req.params.id,
+      {
+        headers: { "Content-Type": "application/json", "authorization": API_KEY},
       }
-
-    } catch(err) {
-      console.error(err.stack);
-      res.json({result: false, error: "Failed to update user details. Please see logs for more details"});
-      next(err);
-    }
+    )
+    const json = await resTemp.json();
+    //if (json.result) {
+      //console.log(`categories enseigne ${ens.id} : ${json.categories}`);
+      //json.categories.map((c) => {if (!categoriesList.includes(c)) {categoriesList.push(c)}})
+    //}
+    categoriesList.push(...json.produits)
+  }
+  res.json({result: true, produits: categoriesList });
 });
 
 /*
@@ -97,39 +75,20 @@ Returns :
     KO = { result: false, error: error_message }
 Description : This route returns all the products for a given category
 */
-router.get('/:id', 
+router.get('/details/:id', 
   async function (req, res, next) {
   // validate params: email and password are mandatory
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      res.json({result: false, error: result.array()})
-      return
-    }
     try {
-      const userDetails = await User.findOne({
-        _id: req.body.userId
-      });
-      
-      if (userDetails === null){
-        res.json({result: false, error: `User with id ${req.body.userId} does not exist`});
-        return;
-      }
-      
-      const hash = bcrypt.hashSync(req.body.password, HASH_ROUNDS);
-
-      const updatedData = await User.updateOne(
-        { _id: req.body.userId },
-        { motDePasse: hash }
-      );
-      if ( updatedData.acknowledged) {
-        res.json({ result: true, message: "User password successfully updated" });
+      const resTemp = await fetch(produitBackend + '/' + req.params.id);
+      const resJson = await resTemp.json();
+      if (!resJson.result ){
+        res.json({result: false, error: `Failed to produit with id ${req.params.id} : error from backend : ${resJson.error}`});
       } else {
-        res.json({ result: false, message: "The password has not been updated" });
+        res.json({ result: true, produit: resJson.produit });
       }
-
     } catch(err) {
       console.error(err.stack);
-      res.json({result: false, error: "Failed to update user password. Please see logs for more details"});
+      res.json({result: false, error: "Failed to get produit detalils. Please see logs for more details"});
       next(err);
     }
 });
