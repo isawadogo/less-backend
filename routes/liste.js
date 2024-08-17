2d5c1e10b0642d2e2560302687d224a5f3f4aab3
Ypolites <manon.bonnafous@outlook.com>
2024-08-17 15:53:26 -0400

    branche budget

--- routes/liste.js
+++ routes/liste.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* Route : GET - User budget - /utilisateur/budget/:token
-IN : user token
+/* Route : GET - List Budget by cathegories - /liste/budget/:month
+IN : current month
 Returns :
-  OK = { result: true, budget: UserBudgetFromMongoDB }
+  OK = { result: true, monthBudget: the month budget }
   KO = { result: false, error: error_message }
 
-Description : This route retrieves user's budget detail with user token
+Description : This route retrieves the spending by categori for the current month
 */
+
+// 1 - faire une route qui récupère les listes par mois
+
+// 2 -  faire une route qui récupère les montants par catégories
 
 modules.exports = router;
