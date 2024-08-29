3091c964aa86c1dbc0d6952a61a3dfb5d44c909c
isawadogo <sidbewende15@gmail.com>
2024-08-29 14:45:55 +0200

    implement comparaision in the backend

--- routes/listes.js
+++ routes/listes.js
@@ -2,12 +2,143 @@ var express = require('express');
 var router = express.Router();
 
 const { body, validationResult } = require('express-validator');
+const { produitBackend, enseignesList, enseigneBackend } = require('../modules/config')
 
 require('../models/connection');
 //const User = require('../models/utilisateur');
 const User = require('../models/utilisateur');
 const Liste = require('../models/liste');
 const API_KEY = process.env.ENSEIGNE_API_KEY;
+
+router.post('/calcul',
+  body('enseignes').notEmpty(),
+  body('criteres').notEmpty(),
+  body('produits').notEmpty(),
+  async function (req, res, next) {
+  // validate params: nom and utilisateur are mandatory
+   const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.json({result: false, error: result.array()})
+      return
+  }
+  const { criteres, produits, enseignes } = req.body;
+  console.log('Data received : ', req.body)
+    // get all the produits matching criteres grouped by enseigne
+    var results = [];
+    const resultatsProduits= []
+    for (const produit of produits) {
+      const reqURL = produitBackend + '/categories/' + produit.categorie + '?nomProduit=' + produit.nom +'&page=1&limit=1000' 
+      try {
+        const resTemp = await fetch(reqURL,
+        {
+          headers: { "Content-Type": "application/json", "authorization": API_KEY},
+        });
+        const json = await resTemp.json();
+        if (json.result) {
+          for (const critereNom of criteres) {
+            let produitsMatch = [];
+            switch (critereNom) {
+              case 'bio':
+              case 'faibleEmpreinte':
+              case 'premierPrix':
+                produitsMatch = json.produits.filter((e) => e[critereNom] === true )
+                //results.push({ critere: critereNom, produitsMatch, nom: produit.nom, categorie: produit.categorie });
+                break;
+              case 'faibleEnMatiereGrasse':
+                produitsMatch = json.produits.filter((e) => e.tauxDeMatiereGrasse <= 10 )
+                //results.push({ critere: critereNom, produitsMatch, nom: produit.nom, categorie: produit.categorie });
+                break;
+              case 'faibleEnSucre':
+                produitsMatch = json.produits.filter((e) => e.tauxDeSucre <= 10 )
+                //results.push({ critere: critereNom, produitsMatch, nom: produit.nom, categorie: produit.categorie });
+                break;
+              case 'distance':
+                // get distance from user address to every enseigne
+
+                // get the produits of enseignes that satify the conditions
+
+                console.log('critere distance');
+              break;
+            }
+            let resultats = [];
+            for (const enseigne of enseignes) {
+            //enseignes.forEach(function(enseigne) {
+              const enseigneProduits = produitsMatch.filter((p) => p.enseigne._id === enseigne._id);
+              //console.log(`enseignes ${enseigne.enseigneNom}, critere : ${critereNom}, produits : ${JSON.stringify(enseigneProduits)}`)
+              const enseigneProduitsCount = enseigneProduits.length;
+              const ponderation = enseigneProduitsCount === 0 ? 0 : 1;
+              const produitEnseigne = enseigneProduitsCount === 0 ? json.produits[0] : enseigneProduits[0];
+              results.push({ 
+                enseigneId: enseigne._id, 
+                enseigneNom: enseigne.nom, 
+                nomProduit: produit.nom, 
+                categorie: produit.categorie,
+                ponderation: ponderation,
+                critere: critereNom,
+                produit: produitEnseigne,
+              })
+            }
+          }//, resultatCriteres
+        }
+      } catch(err) {
+        console.error(err.stack);
+        res.json({result: false, error: "Failed to get produits list. Please see logs for more details"});
+        next(err);
+      }
+    }
+    //console.log('RES PROD : ', results);
+
+   // res.json({result: true, results});
+    const listeRes = results
+    //const criteresUtilisateur = [ ...new Set(listeRes.resultat.filter((f) => f.critere !== '_id' && user.criteres[f.critere] ).map((c) => c.critere)) ]
+    const criteresUtilisateur = criteres;
+    const produitsSelected = produits;
+    const catSelected = [...new Set(produitsSelected.map((e) => e.categorie))].map((e, i) => {return {nom: e, id: i}})
+    //console.log('LISTES RES : ', listeRes);
+    const resultComparaison = listeRes.reduce((a, v, i, res) => {
+      const isKeyPresent = a.some((k) => k.enseigneId == v.enseigneId);
+      if (!isKeyPresent) {
+        let tmp = { 
+          enseigneId: v.enseigneId,
+          nom: v.enseigneNom,
+          criteresPercentage: [],
+          produits: [],
+        }
+        criteresUtilisateur.map((c) => {
+          const nbCrit = res.filter((r) => r.critere === c && v.enseigneId === r.enseigneId);
+          const poids = nbCrit.reduce((acc, val) => acc + val.ponderation, 0);
+          const totalCrit = produitsSelected.length;
+          const moyenne = totalCrit !== 0 ? (poids/nbCrit.length)*100: 0;
+          tmp['criteresPercentage'].push({nom: c, note: moyenne});
+        });
+        catSelected.map((d) => {
+          res.filter((b) => b.enseigneId === v.enseigneId && v.categorie === d.nom).map((f) => {
+            if (!tmp['produits'].some((x) => x.nomProduit === f.nomProduit )) {
+              let crits = [];
+              if (Object.keys(f.produit).length > 0) {
+                criteresUtilisateur.map((c) => {
+                  if (f.produit[c]) {
+                    crits.push(c);
+                  }
+                })
+                tmp['produits'].push({categorie: f.categorie, nomProduit: f.nomProduit, produit: f.produit, criteres: crits, quantite: f.quantite});
+              } /*else {
+                  // Si aucun produit ne match aucun des criteres de l'utilisateur, recuperer le produit le moins cher
+              }*/
+          }
+          });
+            if (tmp['produits']) {
+              tmp['produits'].sort((c, d) => d.criteres.length - c.criteres.length);
+            }
+        });
+        tmp['conformite'] = (tmp.criteresPercentage.reduce((acc, val) => acc + val.note, 0)/criteresUtilisateur.length).toFixed(2);
+        //console.log('TMP : ', tmp);
+        return a.concat(tmp)
+      } else { return a}
+    }, []);
+    console.log('Result status : Data sent - ', resultComparaison)
+   res.json({result: true, resultComparaison});
+});
 
 /*
 Route : GET - Get -liste details /getListe/:nom
